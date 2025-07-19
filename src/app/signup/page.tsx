90d65a4079e8eae171959a1a8ec8fe831959460e
김이서 <12305025k@gmail.com>
2025-07-20 00:01:17 +0900

    WIP: 회원가입 페이지 테스트

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,73 +1,69 @@
-import useFormField from '@/hooks/useFormField';
+// import useFormField from '@/hooks/useFormField';
 
-const signup = () => {
-  const { values, valid, handleInputChange, handleInputBlur } = useFormField(
-    ['email', 'nickName', 'password', 'passwordVerify'],
-    'signup',
-  );
-  return (
-    <>
-      <section>
-        <form>
-          <AuthField>
-            <AuthField.label htmlFor='email'>이메일</AuthField.label>
-            <AuthField.input
-              id='email'
-              placeholder='이메일을 입력해 주세요'
-              ariaLabel='이메일 입력 칸'
-              type='email'
-              value={values.email}
-              onChange={handleInputChange}
-              onBlur={handleInputBlur}
-            />
-            <AuthField.valid valid={valid.accountId}>{valid.accountId}</AuthField.valid>
-          </AuthField>
+// const signup = () => {
+//   const { values, valid, handleInputChange, handleInputBlur } = useFormField(
+//     ['email', 'nickName', 'password', 'passwordVerify'],
+//     'signup',
+//   );
+//   return (
+//     <>
+//       <section>
+//         <form>
+//           <AuthField>
+//             <AuthField.label htmlFor='email'>이메일</AuthField.label>
+//             <AuthField.input
+//               id='email'
+//               placeholder='이메일을 입력해 주세요'
+//               ariaLabel='이메일 입력 칸'
+//               type='email'
+//               value={values.email}
+//               onChange={handleInputChange}
+//               onBlur={handleInputBlur}
+//             />
+//             <AuthField.valid valid={valid.accountId}>{valid.accountId}</AuthField.valid>
+//           </AuthField>
 
-          <AuthField>
-            <AuthField.label htmlFor='nickName'>닉네임</AuthField.label>
-            <AuthField.input
-              id='nickName'
-              placeholder='닉네임을 입력해 주세요'
-              ariaLabel='닉네임 입력 칸'
-              type='text'
-              value={values.nickName}
-            />
-            <AuthField.valid valid={valid.nickName}>{valid.nickName}</AuthField.valid>
-          </AuthField>
+//           <AuthField>
+//             <AuthField.label htmlFor='nickName'>닉네임</AuthField.label>
+//             <AuthField.input
+//               id='nickName'
+//               placeholder='닉네임을 입력해 주세요'
+//               ariaLabel='닉네임 입력 칸'
+//               type='text'
+//               value={values.nickName}
+//             />
+//             <AuthField.valid valid={valid.nickName}>{valid.nickName}</AuthField.valid>
+//           </AuthField>
 
-          <AuthField>
-            <AuthField.label htmlFor='password'>비밀번호</AuthField.label>
-            <AuthField.input
-              id='password'
-              placeholder='8자 이상 입력해 주세요'
-              ariaLabel='비밀번호 입력 칸'
-              type='password'
-              value={values.password}
-            />
-            <AuthField.valid valid={valid.password}>{valid.password}</AuthField.valid>
-          </AuthField>
+//           <AuthField>
+//             <AuthField.label htmlFor='password'>비밀번호</AuthField.label>
+//             <AuthField.input
+//               id='password'
+//               placeholder='8자 이상 입력해 주세요'
+//               ariaLabel='비밀번호 입력 칸'
+//               type='password'
+//               value={values.password}
+//             />
+//             <AuthField.valid valid={valid.password}>{valid.password}</AuthField.valid>
+//           </AuthField>
 
-          <AuthField>
-            <AuthField.label htmlFor='passwordVerify'>비밀번호 확인</AuthField.label>
-            <AuthField.input
-              id='passwordVerify'
-              placeholder='비밀번호를 한번 더 입력해 주세요'
-              ariaLabel='비밀번호 확인 입력칸'
-              type='password'
-              value={values.passwordVerify}
-            />
-            <AuthField.valid valid={valid.passwordVerify}>{valid.passwordVerify}</AuthField.valid>
-          </AuthField>
-        </form>
-        <div></div>
-      </section>
-    </>
-  );
-};
-export default signup;
+//           <AuthField>
+//             <AuthField.label htmlFor='passwordVerify'>비밀번호 확인</AuthField.label>
+//             <AuthField.input
+//               id='passwordVerify'
+//               placeholder='비밀번호를 한번 더 입력해 주세요'
+//               ariaLabel='비밀번호 확인 입력칸'
+//               type='password'
+//               value={values.passwordVerify}
+//             />
+//             <AuthField.valid valid={valid.passwordVerify}>{valid.passwordVerify}</AuthField.valid>
+//           </AuthField>
+//         </form>
+//         <div></div>
+//       </section>
+//     </>
+//   );
+// };
+// export default signup;
 
-
-
-
-A ㅏ ............
-
+// A ㅏ ............
--- src/hooks/useFormField.tsx
+++ src/hooks/useFormField.tsx
@@ -1,119 +1,119 @@
-import { useState, useCallback } from 'react';
-import {
-  FieldName,
-  ValidationRules,
-  ValidationResult,
-  FormState,
-  UseFormFieldResult,
-} from '@/hooks/types';
+// import { useState, useCallback } from 'react';
+// import {
+//   FieldName,
+//   ValidationRules,
+//   ValidationResult,
+//   FormState,
+//   UseFormFieldResult,
+// } from '@/hooks/types';
 
-const fieldNames: FieldName[] = ['password', 'passwordVerify', 'email', 'nickName'];
+// const fieldNames: FieldName[] = ['password', 'passwordVerify', 'email', 'nickName'];
 
-const validationRules: { [key: string]: ValidationRules } = {
-  password: {
-    required: true,
-    minLength: 8,
-  },
-  passwordVerify: {
-    required: true,
-    match: 'password',
-  },
-  nickName: {
-    required: true,
-    minLength: 2,
-  },
-  email: {
-    required: true,
-    email: true,
-  },
-};
+// const validationRules: { [key: string]: ValidationRules } = {
+//   password: {
+//     required: true,
+//     minLength: 8,
+//   },
+//   passwordVerify: {
+//     required: true,
+//     match: 'password',
+//   },
+//   nickName: {
+//     required: true,
+//     minLength: 2,
+//   },
+//   email: {
+//     required: true,
+//     email: true,
+//   },
+// };
 
-const useFormField = <T extends FieldName>(initialFields: T[]): UseFormFieldResult<T> => {
-  const initialFieldStates = initialFields.reduce((acc, name) => {
-    acc[name] = { value: '', valid: null, message: '' };
-    return acc;
-  }, {} as FormState<T>);
+// const useFormField = <T extends FieldName>(initialFields: T[]): UseFormFieldResult<T> => {
+//   const initialFieldStates = initialFields.reduce((acc, name) => {
+//     acc[name] = { value: '', valid: null, message: '' };
+//     return acc;
+//   }, {} as FormState<T>);
 
-  const [fieldStates, setFieldStates] = useState<FormState<T>>(initialFieldStates);
+//   const [fieldStates, setFieldStates] = useState<FormState<T>>(initialFieldStates);
 
-  const validateField = useCallback(
-    (name: T, value: string): ValidationResult => {
-      const rules = validationRules[name];
-      if (!rules) return { valid: null, message: '' };
+//   const validateField = useCallback(
+//     (name: T, value: string): ValidationResult => {
+//       const rules = validationRules[name];
+//       if (!rules) return { valid: null, message: '' };
 
-      if (rules.required && !value) {
-        return { valid: false, message: '필수 입력 항목입니다.' };
-      }
-      if (rules.minLength && value.length < (rules.minLength as number)) {
-        return { valid: false, message: `${rules.minLength}}자 이상이어야 합니다.` };
-      }
+//       if (rules.required && !value) {
+//         return { valid: false, message: '필수 입력 항목입니다.' };
+//       }
+//       if (rules.minLength && value.length < (rules.minLength as number)) {
+//         return { valid: false, message: `${rules.minLength}}자 이상이어야 합니다.` };
+//       }
 
-      if (rules.match && value !== fieldStates[rules.match].value) {
-        return { valid: false, message: `${name}이(가) 일치하지 않습니다.` };
-      }
+//       if (rules.match && value !== fieldStates[rules.match].value) {
+//         return { valid: false, message: `${name}이(가) 일치하지 않습니다.` };
+//       }
 
-      if (rules.email && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)) {
-        return { valid: false, message: '올바른 이메일 형식이 아닙니다.' };
-      }
+//       if (rules.email && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)) {
+//         return { valid: false, message: '올바른 이메일 형식이 아닙니다.' };
+//       }
 
-      return { valid: true, message: '' };
-    },
-    [fieldStates],
-  );
+//       return { valid: true, message: '' };
+//     },
+//     [fieldStates],
+//   );
 
-  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    const fieldName = name as T;
+//   const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+//     const { name, value } = e.target;
+//     const fieldName = name as T;
 
-    setFieldStates((prevFieldStates) => ({
-      ...prevFieldStates,
-      [fieldName]: { ...prevFieldStates[fieldName], value: value },
-    }));
-  }, []);
+//     setFieldStates((prevFieldStates) => ({
+//       ...prevFieldStates,
+//       [fieldName]: { ...prevFieldStates[fieldName], value: value },
+//     }));
+//   }, []);
 
-  const values: Recort<T, string> = Object.fromEntries(
-    Object.entries(fieldStates).map(([key, value]) => [key, value.value]),
-  ) as Record<T, string>;
+//   const values: Recort<T, string> = Object.fromEntries(
+//     Object.entries(fieldStates).map(([key, value]) => [key, value.value]),
+//   ) as Record<T, string>;
 
-  const valids: Record<T, boolean | null> = Object.fromEntries(
-    Object.entries(fieldStates).map(([key, value]) => [key, value.valid]),
-  ) as Record<T, boolean | null>;
+//   const valids: Record<T, boolean | null> = Object.fromEntries(
+//     Object.entries(fieldStates).map(([key, value]) => [key, value.valid]),
+//   ) as Record<T, boolean | null>;
 
-  const message: Record<T, string> = Object.fromEntries(
-    Object,
-    entries(fieldStates).map(([key, value]) => [key, value.message]),
-  ) as Record<T, string>;
+//   const message: Record<T, string> = Object.fromEntries(
+//     Object,
+//     entries(fieldStates).map(([key, value]) => [key, value.message]),
+//   ) as Record<T, string>;
 
-  const isSubmitEnable = Object.values(fieldStates).every(
-    (fieldState) => fieldState.valid === true,
-  );
+//   const isSubmitEnable = Object.values(fieldStates).every(
+//     (fieldState) => fieldState.valid === true,
+//   );
 
-  const handleBlur = useCallback(
-    (e: React.FocusEvent<HTMLInputElement>) => {
-      const { id, value } = e.target;
-      const fieldName = id as T;
-      const validationResult = validateField(fieldName, value);
+//   const handleBlur = useCallback(
+//     (e: React.FocusEvent<HTMLInputElement>) => {
+//       const { id, value } = e.target;
+//       const fieldName = id as T;
+//       const validationResult = validateField(fieldName, value);
 
-      setFieldStates((prevFieldStates) => ({
-        ...prevFieldStates,
-        [fieldName]: {
-          ...prevFieldStates[fieldName],
-          valid: validationResult.valid,
-          message: validationResult.message,
-        },
-      }));
-    },
-    [validateField],
-  );
+//       setFieldStates((prevFieldStates) => ({
+//         ...prevFieldStates,
+//         [fieldName]: {
+//           ...prevFieldStates[fieldName],
+//           valid: validationResult.valid,
+//           message: validationResult.message,
+//         },
+//       }));
+//     },
+//     [validateField],
+//   );
 
-  return {
-    value: validationRules,
-    valids: validationRules,
-    message: message,
-    isSubmitEnable,
-    handleChange,
-    handleBlur,
-  };
-};
+//   return {
+//     value: validationRules,
+//     valids: validationRules,
+//     message: message,
+//     isSubmitEnable,
+//     handleChange,
+//     handleBlur,
+//   };
+// };
 
-export default useFormField;
+// export default useFormField;
