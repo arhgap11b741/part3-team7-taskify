30c9bf75477bf433cdd3cd770d55681164da28e0
Squarecat-meow <Yozumina@serafuku.moe>
2025-07-22 14:37:05 +0900

    feat(components): UserDropdown 작성, Type과 Context 리팩토링

--- src/components/dropdown/DropdownContext.ts
+++ src/components/dropdown/DropdownContext.ts
@@ -1,5 +1,5 @@
 import { createContext, useContext } from 'react';
-import { DropdownContextType } from './DropdownTypes';
+import { DropdownContextType, UserDropdownContextType } from './DropdownTypes';
 
 const DropdownContextDefaultValues: DropdownContextType = {
   isOpen: false,
@@ -9,10 +9,27 @@ const DropdownContextDefaultValues: DropdownContextType = {
   setSelectedItem: () => {},
 };
 
+const UserDropdownContextDefaultValues: UserDropdownContextType = {
+  isOpen: false,
+  selectedItem: null,
+  toggleDropdown: () => {},
+  closeDropdown: () => {},
+  setSelectedItem: () => {},
+};
+
 export const DropdownContext = createContext(DropdownContextDefaultValues);
+export const UserDropdownContext = createContext(UserDropdownContextDefaultValues);
 
 export const useDropdownContext = (): DropdownContextType => {
   const context = useContext(DropdownContext);
+  if (context === undefined) {
+    console.error('Dropdown Context는 Dropdown.Root안에서 사용해주세요!');
+  }
+  return context;
+};
+
+export const useUserDropdownContext = (): UserDropdownContextType => {
+  const context = useContext(UserDropdownContext);
   if (context === undefined) {
     console.error('Dropdown Context는 Dropdown.Root안에서 사용해주세요!');
   }
--- src/components/dropdown/DropdownTypes.ts
+++ src/components/dropdown/DropdownTypes.ts
@@ -1,7 +1,17 @@
+import { UserType } from '../chip/UserChip';
+
 export interface DropdownContextType {
   isOpen: boolean;
   selectedItem: string | null;
   toggleDropdown: () => void;
   closeDropdown: () => void;
   setSelectedItem: (item: string | null) => void;
+}
+
+export interface UserDropdownContextType {
+  isOpen: boolean;
+  selectedItem: UserType | null;
+  toggleDropdown: () => void;
+  closeDropdown: () => void;
+  setSelectedItem: (item: UserType | null) => void;
 }
