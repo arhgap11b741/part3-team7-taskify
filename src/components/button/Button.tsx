cc37f76f90aca6fb692a25b02c873b4826f9d5a7
Squarecat-meow <Yozumina@serafuku.moe>
2025-07-24 10:23:36 +0900

    fix(Button): 버튼 코드 리팩토링, extrasmall 추가

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -15,6 +15,19 @@ type FinalButtonType =
   | ConditionalButtonType<'small'>
   | ConditionalButtonType<'extraSmall'>;
 
+const baseClasses = 'rounded-sm';
+const sizeClasses = {
+  large: 'py-3.5 w-[351px] border-none cursor-pointer lg:w-[520px]',
+  small: 'px-7 py-2 cursor-pointer lg:px-5.5 md:px-6',
+  extraSmall: 'px-2 md:px-3 py-1 md:py-2 cursor-pointer',
+};
+const typeClasses = {
+  primary: 'bg-violet-500 text-white border-none cursor-pointer',
+  outline: 'bg-white border border-gray-200 text-violet-500 cursor-pointer',
+  disabled: 'bg-gray-300 text-white cursor-not-allowed',
+  gnb: 'flex items-center gap-2 text-gray-600 bg-white border border-gray-200 rounded-xl',
+};
+
 export const Button: FC<FinalButtonType> = ({
   size = 'small',
   type,
@@ -23,19 +36,6 @@ export const Button: FC<FinalButtonType> = ({
   disabled,
   className,
 }) => {
-  const baseClasses = 'rounded-sm';
-  const sizeClasses = {
-    large: 'py-3.5 w-[351px] border-none cursor-pointer lg:w-[520px]',
-    small: 'px-7 py-2 cursor-pointer lg:px-5.5 md:px-6',
-    extraSmall: 'px-3 py-2 cursor-pointer',
-  };
-  const typeClasses = {
-    primary: 'bg-violet-500 text-white border-none cursor-pointer',
-    outline: 'bg-white border border-gray-200 text-violet-500 cursor-pointer',
-    disabled: 'bg-gray-300 text-white cursor-not-allowed',
-    gnb: 'flex items-center gap-2 text-gray-600 bg-white border border-gray-200 rounded-xl',
-  };
-
   const classes = `${baseClasses} ${sizeClasses[size]} ${typeClasses[type]} ${className || ''}`;
 
   return (
