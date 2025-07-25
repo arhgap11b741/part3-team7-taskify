beaf65fe334ec6f7a9c20da7eca2a4c975872482
Squarecat-meow <Yozumina@serafuku.moe>
2025-07-25 17:07:18 +0900

    fix(directory): 폴더 구조 보기 쉽게 변경

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -3,8 +3,8 @@ import SnbNav from '@/components/SnbNav';
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className='flex'>
-      <SnbNav /> {/* 왼쪽 사이드 내비 */}
-      <main className='flex-1'>{children}</main>
+      <SnbNav />
+      {children}
     </div>
   );
 }
