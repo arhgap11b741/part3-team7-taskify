58d1664e2bdc8feb89e5dce40c4ba956623ab947
김이서 <12305025k@gmail.com>
2025-07-26 15:47:59 +0900

    feat:TaskForm

---
+++ src/utils/formatDueDate.ts
@@ -0,0 +1,13 @@
+export const formatDueDate = (date: Date | null) => {
+  if (!date) return '';
+
+  const pad = (n: number) => n.toString().padStart(2, '0');
+
+  const year = date.getFullYear();
+  const month = pad(date.getMonth() + 1); // 0부터 시작
+  const day = pad(date.getDate());
+  const hours = pad(date.getHours());
+  const minutes = pad(date.getMinutes());
+
+  return `${year}-${month}-${day} ${hours}:${minutes}`;
+};
